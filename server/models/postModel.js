fc6232d52ac7128563b013eb2487bc790f17dcc1
Rushikesh <pardhikar26@gmail.com>
2025-02-26 05:10:13 +0530

    post controllers done

--- server/models/postModel.js
+++ server/models/postModel.js
@@ -5,7 +5,7 @@ const postSchema = new Schema({
     category:{type:String, enum:["Agriculture","Business", "Education", "Entertainment", "Art", "Investment", "Uncategorized", "Weather"], message:"{VALUE is not supported}"},
     description: {type:String, required: true},
     creator: {type:Schema.Types.ObjectId, ref:"User"},
-    title:{type:String, required: true}, 
+    thumbnail:{type:String, required: true}, 
 },{timestamps:true})
 
 module.exports = model("Post", postSchema)
