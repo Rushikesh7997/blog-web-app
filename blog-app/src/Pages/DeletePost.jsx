3c708477410697e770317481bdf55d6a485a1790
Rushikesh <pardhikar26@gmail.com>
2025-02-27 23:10:38 +0530

    post details update with optimize

--- blog-app/src/Pages/DeletePost.jsx
+++ blog-app/src/Pages/DeletePost.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from 'react'
 import { UserContext } from '../context/userContext';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export const DeletePost = () => {
 
@@ -17,6 +17,6 @@ export const DeletePost = () => {
   },[])
 
   return (
-    <div>DeletePost</div>
+    <Link className='btn sm danger'>Delete</Link>
   )
 }
--- blog-app/src/Pages/PostDetail.jsx
+++ blog-app/src/Pages/PostDetail.jsx
@@ -1,28 +1,55 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { PostAuthor } from '../Components/PostAuthor'
-import { Link } from 'react-router-dom'
-import Thumbnail from "../images/blog22.jpg"
+import { Link, useParams } from 'react-router-dom'
+import {Loader} from '../Components/Loader'
+import {DeletePost} from './DeletePost'
+import { UserContext } from '../context/userContext'
+import axios from 'axios'
  
 export const PostDetail = () => {
+
+  const {id} = useParams();
+  const [post, setPost] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const {currentUser} = useContext(UserContext);
+
+
+  useEffect(()=>{
+    const getPost = async ()=>{
+      setIsLoading(true);
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/posts/${id}`)
+        setPost(response.data)
+      } catch (error) {
+        setError(error)
+      }
+      setIsLoading(false);
+    }
+    getPost();
+  },[])
+
+  if(isLoading){
+    return <Loader/>
+  }
+
   return (
     <section className="post-detail">
-      <div className='container post-detail__container'>
+      {error && <p className='error'>{error}</p>}
+      {post && <div className='container post-detail__container'>
         <div className='post-detail__header'>
-          <PostAuthor/>
-          <div className='post-detail__buttons'>
+          <PostAuthor authorID={post.creator} createdAt={post.createdAt}/>
+          {currentUser?.id == post?.creator && <div className='post-detail__buttons'>
             <Link to={`/posts/werwer/edit`} className='btn sm primary'>Edit</Link>
-            <Link to={`/posts/werwer/delete`} className='btn sm danger'>Delete</Link>
-          </div>
+            <DeletePost postId={id}/> 
+          </div>}
         </div>
-        <h1>This is Post Title!</h1>
+        <h1>{post.title}</h1>
         <div className='post-detail__thumbnail'>
-          <img src={Thumbnail} alt="" />
+          <img src={`${process.env.REACT_APP_ASSETS_URL}/uploads/${post.thumbnail}`} alt="" />
         </div>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet sequi explicabo, aliquam placeat porro tempora numquam ratione id voluptatibus sint, consectetur velit quos fugit est, at alias cumque? Recusandae aliquam doloribus accusantium porro beatae ab ratione voluptates eius labore quisquam!</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio velit recusandae suscipit nulla laborum! Deserunt blanditiis quod vitae modi reiciendis neque eveniet, autem, ea laudantium, velit corrupti rem ut provident perferendis. Porro, praesentium? Quis impedit praesentium, sequi nihil fugiat a iusto aliquid ducimus corporis error placeat quasi mollitia veritatis. Voluptatum labore voluptatem dolorem. Tempore, laboriosam?</p>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos officiis tempora, optio sequi fugit fuga! Vitae inventore eveniet illum ut aperiam dolorem, quam, asperiores facilis quod unde doloribus officiis deserunt ullam provident eligendi odio iste. Facere repellat necessitatibus aliquam quisquam a cupiditate deserunt dolorem rem sapiente nihil aliquid id odit sequi ut modi provident voluptas eaque quo, molestiae debitis. Tempora, ad fugit animi cum mollitia, voluptatum eligendi eaque soluta eum modi ipsam eius culpa accusamus consequatur unde quidem. Illum impedit sed itaque vitae aspernatur sapiente beatae officia veniam eum enim ipsa nulla praesentium cum sequi illo doloribus explicabo, deserunt expedita dolore architecto voluptatibus suscipit labore nobis nesciunt! Totam eum mollitia atque!</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, enim quos? Quasi alias minus autem rerum voluptatibus nulla quas dolor, placeat, earum in molestias, nihil nobis quis eveniet repellat labore provident sed totam id accusamus. Necessitatibus voluptates non cum maiores!</p>
-      </div>
+        <p dangerouslySetInnerHTML={{__html: post.description}}></p>
+      </div>}
     </section>
   )
 }
